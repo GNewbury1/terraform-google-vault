2de570e49f2dd72e50a9738dc0fefd4dd664a0ce
Guy Newbury <guy.newbury@cloudbuy.com>
2018-09-03 10:51:37 +0100

    Added instance_group to output

--- modules/vault-cluster/outputs.tf
+++ modules/vault-cluster/outputs.tf
@@ -2,6 +2,10 @@ output "cluster_tag_name" {
   value = "${var.cluster_name}"
 }
 
+output "instance_group" {
+  value = "${google_compute_instance_group_manager.vault.instance_group}"
+}
+
 output "instance_group_id" {
   value = "${google_compute_instance_group_manager.vault.id}"
 }
